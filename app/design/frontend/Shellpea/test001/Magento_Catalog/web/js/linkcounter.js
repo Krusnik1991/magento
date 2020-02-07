72c61e2df67e258cf7083bed3803196c10b2b6fb
krusnik1991 <krustnik1991@mail.ru>
2020-02-07 11:22:32 +0300

    added node by modify custom func

--- app/design/frontend/Shellpea/test001/Magento_Catalog/web/js/linkcounter.js
+++ app/design/frontend/Shellpea/test001/Magento_Catalog/web/js/linkcounter.js
@@ -3,8 +3,11 @@ define(
     function($) {
         "use strict";
         return function() {
-            let quantity = $(".footer.links").find('a').length;
-            alert(quantity);
+            let quantity = $(".footer.links").find('a').length + " " + "ссылок";
+            let newDiv = document.createElement("div");
+            newDiv.innerHTML = quantity;
+            let my_div = $(".footer.links");
+            $(newDiv).insertAfter(my_div);
         };
     }
 );
